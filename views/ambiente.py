1ff927eb43482f43528ce8aa3d10d2a2f2f990da
Apoeip43 <54356541+Apoeip43@users.noreply.github.com>
2019-11-18 13:48:21 -0300

    Janela de info ambiente
    atualmente só atualiza a temperatura, e deve ser atualizada com a função de leitura do sensor quando colocado no raspberry.

--- views/ambiente.py
+++ views/ambiente.py
@@ -1,2 +1,56 @@
-class Ambiente:
-    pass
+from tkinter import *
+from tkinter import ttk, messagebox
+import threading as th 
+import numpy as np 
+
+
+class Ambiente(Frame):
+	
+	font = ("Calibri", 12, 'bold')
+
+	def __init__(self, master = None, bg = 'light blue', **kargs):
+
+		self.master = master
+		self.color = bg
+
+		super().__init__(master, bg = self.color, **kargs)
+		# self.__dummy__criabuts()
+		self.criaTemp()
+
+
+	def criaTemp(self):
+
+		self.LabelTexto = Label(self, text = 'Temperatura ambiente: ', font = self.font, bg = self.color)
+		self.LabelTemp = Label(self, text = '0°C', font = self.font, bg = self.color)
+		self.LabelTemp.config(anchor = 'center')
+		self.columnconfigure(0, weight = 1)
+		self.columnconfigure(1, weight = 1)
+		
+		self.LabelTexto.grid(column = 0, row = 0, columnspan = 1, sticky = 'nsew')
+		self.LabelTemp.grid(column = 1, row = 0, columnspan = 1, sticky = 'nsew')
+		
+		self.oldtemp = 25.0
+		self.after(1000, self.updateTemp)
+		
+	def updateTemp(self):
+		add = (np.random.rand()-0.5)*2
+		print(add)
+		newtemp = add+self.oldtemp
+		self.oldtemp = newtemp
+		self.LabelTemp.configure(text = f'{newtemp:.2f}°C')
+		self.after(1000, self.updateTemp)
+
+
+
+
+
+	def __dummy__criabuts(self):
+
+		but1 = Button(self, text = 'Hello')
+		but1.grid(column = 0, row = 0, columnspan = 2, sticky= 'nsew')
+		but2 = Button(self, text = 'world')
+		but2.grid(column = 1, row = 1, columnspan = 4, sticky= 'nsew')
+
+		self.columnconfigure(0, weight = 1)
+		self.columnconfigure(1, weight = 1)
+		self.columnconfigure(2, weight = 1)
--- views/master.py
+++ views/master.py
@@ -1,9 +1,13 @@
 from tkinter import *
-
+import views.ambiente as am 
 
 class Master(Frame):
 
-    def __init__(self, master=None, title="", bg="pink"):
+    def __init__(self, master=None, title="", bg="pink", **kargs):
         self.master = master
-        super().__init__(master)
+        super().__init__(master, **kargs)
         self.master.title(title)
+        self.amb = am.Ambiente(self, bd = 5, relief = "sunken")
+        self.amb.grid(row=0, column=0, columnspan=2, sticky="NSWE", padx=5, pady=5)
+       	self.columnconfigure(0, weight = 1)
+        # self.amb.pack(fill = 'both', expand = 1)
