14af5edc1af4922afa66ef0c8b7c6e02e0de68d7
Ronaldo Luis <sadeluisronaldo@gmail.com>
2019-11-28 01:15:58 -0300

    Configuração visual, troca de segundos por barra crescente

--- views/relogio.py
+++ views/relogio.py
@@ -7,33 +7,35 @@ class Relogio(Frame):
     def troca(self):
         self.hora['text'] = strftime('%H:')
         self.minuto['text'] = strftime('%M')
-        self.segundo['text'] = strftime('%S')
+        # self.segundo['text'] = strftime('%S')
+        self.segundoBar.place(rely=(0.35-datetime.now().second/240), relheight=(datetime.now().second/240))
 
-        self.after(100, self.troca)
+        self.after(500, self.troca)
 
     def __init__(self, master=None, bg=""):
         self.master = master
         super().__init__(master)
 
-        self.posX = 0.08
-        self.posY = 0.15
+        self.posX = 0.01
+        self.posY = 0.105
 
         self.hora = Label()
         self.hora.place(relx=self.posX, rely=self.posY)
-        self.hora['text'] = strftime('%H:')
-        self.hora['font'] = 'Helvita 100 bold'
+        self.hora['font'] = 'Helvita 140 bold'
         self.hora.config(bg=bg, highlightthickness=0, borderwidth=0)
 
         self.minuto = Label()
-        self.minuto.place(relx=self.posX+0.18, rely=self.posY)
-        self.minuto['text'] = strftime('%M:')
-        self.minuto['font'] = 'Helvita 100'
+        self.minuto.place(relx=self.posX+0.25, rely=self.posY)
+        self.minuto['font'] = 'Helvita 140'
         self.minuto.config(bg=bg, highlightthickness=0, borderwidth=0)
 
-        self.segundo = Label()
-        self.segundo.place(relx=self.posX+0.31, rely=self.posY+0.11)
-        self.segundo['text'] = strftime('%S')
-        self.segundo['font'] = 'Helvita 30'
-        self.segundo.config(bg=bg, highlightthickness=0, borderwidth=0)
+        # self.segundo = Label()
+        # self.segundo.place(relx=self.posX+0.43, rely=self.posY+0.16)
+        # self.segundo['font'] = 'Helvita 40'
+        # self.segundo.config(bg=bg, highlightthickness=0, borderwidth=0)
+
+        self.segundoBar = Label()
+        self.segundoBar.place(relx=self.posX+0.45, relwidth=0.02)
+        self.segundoBar.config(bg="#606060")
 
         self.troca()
