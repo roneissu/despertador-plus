f06acc816f18b208fc00a4e4246f6117ab802655
Ronaldo Luis <sadeluisronaldo@gmail.com>
2019-11-28 17:59:30 -0300

    Remove unused variables

--- views/master.py
+++ views/master.py
@@ -11,18 +11,17 @@ class Master(Frame):
         super().__init__(master, **kargs)
         self.master.title(title)
 
-        h = (master.winfo_screenheight()/2)
-        w = (master.winfo_screenwidth()/2)
-
         relogio = Relogio(master, bg=bg)
         relogio.place(relheight=0.5, relwidth=0.5)
         relogio.config(bg=bg)
-        
-        self.amb = Ambiente(self, bd = 5, relief = "sunken")
-        self.amb.grid(row=2, column=2, columnspan=2, sticky="NSWE", padx=5, pady=5)
-       	self.columnconfigure(2, weight = 1)
 
-       	self.prev = Previsao(self, bd = 5, relief = "sunken")
-       	self.prev.grid(row = 1, column = 1, rowspan = 2, sticky = 'nswe', padx = 5, pady = 5)
-       	self.rowconfigure(1, weight = 1)
+        self.amb = Ambiente(self, bd=5, relief="sunken")
+        self.amb.grid(row=2, column=2, columnspan=2,
+                      sticky="NSWE", padx=5, pady=5)
+        self.columnconfigure(2, weight=1)
+
+        self.prev = Previsao(self, bd=5, relief="sunken")
+        self.prev.grid(row=1, column=1, rowspan=2,
+                       sticky='nswe', padx=5, pady=5)
+        self.rowconfigure(1, weight=1)
         # self.amb.pack(fill = 'both', expand = 1)
