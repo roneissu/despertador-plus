81a8f5530ebe525abb2f17c58acdb2e4219a8272
Ronaldo Santos <sadeluisronaldo@gmail.com>
2019-12-09 20:14:04 -0300

    Relogio (#6)
    * Reorganiza a tela
    
    * Reorganiza a tela
    
    * Relogio pronto

--- views/relogio.py
+++ views/relogio.py
@@ -3,40 +3,44 @@ from time import strftime
 from tkinter import *
 
 
+class SegundoBarra(Frame):
+    def troca(self, segundo=0):
+        self.segundoBar.place(rely=0.9-segundo/80, relheight=segundo/80)
+
+
+    def __init__(self, master=None, **kw):
+        super().__init__(master=master, **kw)
+        self.segundoBar = Label(self, bg="#606060")
+        self.segundoBar.place(relx=0.44, rely=0.1, relwidth=0.12)
+
+
 class Relogio(Frame):
     def troca(self):
         self.hora['text'] = strftime('%H:')
         self.minuto['text'] = strftime('%M')
-        # self.segundo['text'] = strftime('%S')
-        self.segundoBar.place(
-            rely=(0.35-datetime.now().second/240), relheight=(datetime.now().second/240))
+        self.segundo.troca(datetime.now().second)
 
         self.after(500, self.troca)
 
-    def __init__(self, master=None, bg=""):
+    def __init__(self, master=None, **kwargs):
         self.master = master
-        super().__init__(master)
+        super().__init__(master, kwargs)
 
-        self.posX = 0.01
-        self.posY = 0.105
-
-        self.hora = Label()
-        self.hora.place(relx=self.posX, rely=self.posY)
+        self.hora = Label(self)
+        self.hora.grid(row=1, column=1, sticky="NSWE")
         self.hora['font'] = 'Helvita 140 bold'
-        self.hora.config(bg=bg, highlightthickness=0, borderwidth=0)
 
-        self.minuto = Label()
-        self.minuto.place(relx=self.posX+0.25, rely=self.posY)
+        self.minuto = Label(self)
+        self.minuto.grid(row=1, column=2, sticky="NSWE")
         self.minuto['font'] = 'Helvita 140'
-        self.minuto.config(bg=bg, highlightthickness=0, borderwidth=0)
 
-        # self.segundo = Label()
-        # self.segundo.place(relx=self.posX+0.43, rely=self.posY+0.16)
-        # self.segundo['font'] = 'Helvita 40'
-        # self.segundo.config(bg=bg, highlightthickness=0, borderwidth=0)
+        self.segundo = SegundoBarra(self)
+        self.segundo.grid(row=0, column=3, rowspan=3, sticky="NSWE")
 
-        self.segundoBar = Label()
-        self.segundoBar.place(relx=self.posX+0.45, relwidth=0.02)
-        self.segundoBar.config(bg="#606060")
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=3)
+        self.rowconfigure(2, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(3, weight=1)
 
         self.troca()
